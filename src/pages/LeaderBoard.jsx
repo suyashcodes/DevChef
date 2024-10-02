0d6005dd1cf92260cf5af2e17bfadabef2688a9b
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2024-10-03 00:10:08 +0530

    updated leaderboard for september competition

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,77 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import DynamicNavbar from '../components/DynamicNavbar'
 import { motion } from 'framer-motion';
+import Papa from "papaparse";
 
 const LeaderBoard = () => {
 
     const [data, setData] = useState([])
+    let rank = 0
+    let preScore = -1;
+    const [search, setSearch] = useState('')
+    const filteredData = data.filter(item => {
+        return item.Name.toLowerCase().includes(search.toLowerCase())
+    })
 
     useEffect(() => {
-      // data fetching
-      const data = {
-        models: [
-            {
-                "hacker_id": 26308361,
-                "language": null,
-                "worst_testcase_time": null,
-                "rank": 1,
-                "score": 5.0,
-                "time_taken": 47.0,
-                "index": 0,
-                "hacker": "suyashdashputre7",
-                "avatar": "https://d1ce3iv5vajdy.cloudfront.net/hackerrank/assets/gravatar.jpg",
-                "country": "India",
-                "school": null,
-                "company": "",
-                "timestamp": 1727185547,
-                "submitted_at": "4 days",
-                "is_multiple_contest": false,
-                "level": 5
-            },
-            {
-                "hacker_id": 19431366,
-                "language": null,
-                "worst_testcase_time": null,
-                "rank": 1,
-                "score": 5.0,
-                "time_taken": 226.0,
-                "index": 1,
-                "hacker": "prathameshsatbh1",
-                "avatar": "https://d1ce3iv5vajdy.cloudfront.net/hackerrank/assets/gravatar.jpg",
-                "country": "India",
-                "school": null,
-                "company": "",
-                "timestamp": 1727185726,
-                "submitted_at": "4 days",
-                "is_multiple_contest": false,
-                "level": 5
-            }
-        ],
-        "page": 1,
-        "total": 2,
-        "current_hacker": null,
-        "available": true,
-        "self_display": true
-    }
+        console.log(filteredData)
 
-    const filteredData = data.models.map(({ rank, score, hacker, hacker_id, avatar, time_taken }) => ({
-        rank,
-        score,
-        hacker,
-        hacker_id,
-        avatar,
-        time_taken
-    }));
+        return () => {
 
-    setData(filteredData)
-    
-      return () => {
-        
-      }
-    }, [])
-    
+        }
+    }, [filteredData])
 
-    
+    useEffect(() => {
+        console.log(search)
+
+        return () => {
+
+        }
+    }, [search])
+
+
+
+    useEffect(() => {
+        // Assuming the CSV file is located in the public folder
+        fetch("leaderboardData/SeptemberLeaderboard - Sheet1.csv")
+            .then((response) => response.text())
+            .then((csvText) => {
+                Papa.parse(csvText, {
+                    header: true,
+                    complete: (results) => {
+                        console.log("Parsed data:", results.data);
+                        setData(results.data);
+                    },
+                    error: (error) => {
+                        console.error("Error while parsing:", error);
+                    },
+                });
+            })
+            .catch((error) => console.error("Error while fetching CSV:", error));
+    }, []);
+
+
+
 
     const boxVariants = {
         initial: {
@@ -81,25 +61,14 @@ const LeaderBoard = () => {
             scale: 1.02,
             backgroundColor: 'white',
             color: 'black',
-            boxShadow: '0px 0px 50px 4px #000'
+            boxShadow: '0px 0px 20px 2px #474340'
         }
     }
 
-    const imageVariants = {
-        initial: {
-            x: 0,
-            y: 0
-        },
-        whileHover: {
-            x: -10,
-            y: -5,
-            boxShadow: '0px 0px 50px 4px #000'
-        }
-    }
 
 
     return (
-        <div className='w-screen h-100svh  bg-[#282523] text-white'>
+        <div className='w-screen min-h-[100svh] bg-[#282523] text-white text-[12px] s600:text-[14px] s800:text-[16px]'>
             <DynamicNavbar
                 links={
                     [
@@ -128,40 +97,77 @@ const LeaderBoard = () => {
                 activeInNavbar={true}
             />
 
-            <section className='w-full max-w-[1000px] gap-y-[20px] flex flex-col  m-auto p-[20px] pt-[100px]'>
+            <section className='w-full max-w-[1200px]  gap-y-[24px] flex flex-col  m-auto p-[20px] pt-[50px]'>
 
-                <div className='ranker-section flex  items-center py-[20px] font-bold text-[110%]'>
-                    <p className='rank w-[10%]  s600:mr-[50px] text-center' >Rank</p>
-                    <div className='flex flex-1  items-center gap-[20px]'>
-                        <p>Participant</p>
-                    </div>
-                    <p className='score w-[20%] text-center'>Score</p>
-                    <p className="time w-[15%] text-center">Time</p>
+                {/* <div className='w-full flex flex-col s600:flex-row justify-center gap-[20px] s600:gap-0 items-center s600:justify-between'>
+                    <h2 className='text-orange-500 text-[130%] font-bold '> September season results</h2>
+                    <input className='text-black max-w-[250px]  w-full focus:outline-none px-[15px] py-[10px] rounded-[10px]' onChange={(e) => setSearch(e.target.value)} value={search} type="text" placeholder='search' />
+                </div> */}
+
+                <div className='ranker-section flex  items-center py-[20px] font-bold '>
+                    <p className='  w-[20px] ml-[10px] s600:ml-0 s600:w-[10%] text-center' >Rank</p>
+                    <p className=' flex-1 text-center'>Name</p>
+                    <p className=' w-[23%] text-center'>Username</p>
+                    <p className=' w-[15%] text-center'>Total Score</p>
+                    <p className=' w-[10%] text-center'>Q1</p>
+                    <p className=' w-[10%] text-center'>Q2</p>
+                    <p className=" w-[10%] text-center ">Q3</p>
                 </div>
                 {
-                data.map((item, index) => (
-                    <motion.div 
-                        key={index} 
-                        className='ranker-section flex  items-center py-[15px] bg-[#383736] rounded-[10px]'
-                        variants={boxVariants}
-                        initial={'intial'}
-                        whileHover={'whileHover'}
-                    >
-                        <p className='rank w-[10%] s600:mr-[50px] text-center' >{item.rank}</p>
-                        <div className='flex flex-1 items-center gap-[20px]'>
-                            <div className='w-[40px] h-[40px] '><motion.img
-                                className='w-full h-full rounded-[50%]' 
-                                src={item.avatar} 
-                                alt="profile" 
-                                variants={imageVariants}
-                            /></div>
-                            <p>{item.hacker}</p>
-                        </div>
-                        <p className='score w-[20%] pl-[10px] text-center'>{item.score}</p>
-                        <p className="time w-[15%] text-center ">{item.time_taken}</p>
-                    </motion.div>
-                ))
-            }</section>
+                    filteredData.map((item, index) => {
+
+                        let backgroundColor = '#383736'
+                        let color = '#fff'
+                        let shadow = ''
+
+                        if (item.Totalscore != preScore) {
+                            rank++
+                            preScore = item.Totalscore
+                        }
+                        if (rank == 1) {
+                            backgroundColor = '#FFD700'
+                            color = '#000'
+                            shadow = '0px 0px 7px 1px #FFD700'
+                        }
+                        else if (rank == 2) {
+                            backgroundColor = '#C0C0C0'
+                            color = '#000'
+                            shadow = '0px 0px 7px 1px #C0C0C0'
+                        }
+                        else if (rank == 3) {
+                            backgroundColor = '#CD7F32'
+                            color = '#000'
+                            shadow = '0px 0px 7px 1px #CD7F32'
+                        }
+
+
+                        return (
+                            <motion.div
+                                key={index}
+                                className='ranker-section flex  items-center py-[22px] rounded-[10px]'
+                                variants={boxVariants}
+                                initial={'intial'}
+                                whileHover={'whileHover'}
+                                style={{ backgroundColor, color, boxShadow: shadow }}
+                            >
+                                <p className='w-[20px] ml-[10px] s600:ml-0 s600:w-[10%]  text-center ' >{rank}</p>
+                                <div className=' w-[23%]  flex justify-center items-center'>
+                                    <div className='w-[25px] h-[13px] relative'>
+                                        {rank == 1 && <img className='w-[25px] h-[13px] rotate-[-30deg] left-[13px] top-[-16px] absolute' src="/icons/golden-crown.png" alt="golden crown" />}
+                                        {rank == 2 && <img className='w-[25px] h-[13px] rotate-[-30deg] left-[13px] top-[-16px] absolute' src="/icons/silver-crown.png" alt="golden crown" />}
+                                        {rank == 3 && <img className='w-[25px] h-[13px] rotate-[-30deg] left-[13px] top-[-16px] absolute' src="/icons/bronze-crown.png" alt="golden crown" />}
+                                    </div>
+                                    <p className='text-center'>{item.Name}</p>
+                                </div>
+                                <p className=' flex-1 text-center'>{item.Username}</p>
+                                <p className=' w-[15%] text-center'>{item.Totalscore}</p>
+                                <p className=' w-[10%] text-center'>{item["Raj's Homework"]}</p>
+                                <p className=' w-[10%] text-center'>{item["Alien Artifacts"]}</p>
+                                <p className=" w-[10%] text-center ">{item["keeper of Mystical key"]}</p>
+                            </motion.div>)
+                    })
+                }
+            </section>
 
         </div>
     )

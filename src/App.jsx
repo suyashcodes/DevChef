c6a2effd511b536288f50656bffa863688f5270c
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-10-02 20:34:16 +0000

    removed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import DevChef from "./pages/DevChef"
 import ScrollToTop from "./components/ScrollToTop"
 import LeaderBoard from "./pages/LeaderBoard"
-window.dataLayer = window.dataLayer || [];
-function ga(args) {
-  dataLayer.push(args);
-}
-ga('js', new Date());
 
-ga('config', 'G-94R4ZWSHRV');
 export default function App() {
     return (
         <Router>
@@ -17,7 +11,6 @@ export default function App() {
                 <Route path="/" element={<DevChef />} />
                 <Route path="/leaderboard" element={<LeaderBoard />} />
             </Routes>
-            
         </Router>
     )
   }

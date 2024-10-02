66e95fc96e700b99e93c0c6ff7b282b4db1e8dcb
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2024-10-02 15:28:46 +0530

    Added initial build

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import DevChef from "./pages/DevChef"
+import ScrollToTop from "./components/ScrollToTop"
+import LeaderBoard from "./pages/LeaderBoard"
+
+export default function App() {
+    return (
+        <Router>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<DevChef />} />
+                <Route path="/leaderboard" element={<LeaderBoard />} />
+            </Routes>
+        </Router>
+    )
+  }

2175e9d636bf42bc137710bdeb407825e484af1b
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-10-02 20:32:07 +0000

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import DevChef from "./pages/DevChef"
 import ScrollToTop from "./components/ScrollToTop"
 import LeaderBoard from "./pages/LeaderBoard"
+window.dataLayer = window.dataLayer || [];
+function ga(args) {
+  dataLayer.push(args);
+}
+ga('js', new Date());
 
+ga('config', 'G-94R4ZWSHRV');
 export default function App() {
     return (
         <Router>

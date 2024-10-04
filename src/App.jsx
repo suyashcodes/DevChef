8a33c24b9d31ea4a58df0e0bb463fb2da48f95dd
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-10-04 04:20:05 +0000

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import DevChef from "./pages/DevChef"
 import ScrollToTop from "./components/ScrollToTop"
 import LeaderBoard from "./pages/LeaderBoard"
-
+import { Analytics } from "@vercel/analytics/react"
 export default function App() {
     return (
         <Router>

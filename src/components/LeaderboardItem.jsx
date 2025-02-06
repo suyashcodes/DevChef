231536acbb8aa83ba995830aa366777bd97539d5
ramkrushnaw04 <ramkrushnaw04@gmail.com>
2025-02-06 14:53:02 +0530

    updated leaderboard ui

--- src/components/LeaderboardItem.jsx
+++ src/components/LeaderboardItem.jsx
@@ -46,7 +46,7 @@ const LeaderboardItem = ({ index, item, keys }) => {
     return (
         <motion.tr
             key={index}
-            className={`${getRankingColor(index)} `}
+            className={`${getRankingColor(index)} h-[50px]`}
             variants={boxVariants}
             initial={'intial'}
             whileHover={'whileHover'}
@@ -56,7 +56,7 @@ const LeaderboardItem = ({ index, item, keys }) => {
                 // for widescreen`
                 keys.map((key, index) => {
                     // if(!key.toLowerCase().startsWith('q'))
-                        return <td key={index} className={` text-[13px] px-4 py-1 s1000:py-4 border-none ${index == 0 ? 'rounded-l-xl' : ''} ${index == keys.length - 1 ? 'rounded-r-xl' : ''}`}>{item[key]}</td>
+                        return <td key={index} className={` text-[13px] px-4 border-none ${index == 0 ? 'rounded-l-xl' : ''} ${index == keys.length - 1 ? 'rounded-r-xl' : ''}`}>{item[key]}</td>
                 })
 
                 // for mobile
